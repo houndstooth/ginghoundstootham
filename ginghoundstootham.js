64b747cf71cec72f1c388af3e8448b273e598fb7
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-03-05 15:19:28 -0800

    global color version

--- ginghoundstootham.js
+++ ginghoundstootham.js
@@ -1,133 +1,79 @@
 const canvas = document.querySelector('.canvas');
 const ctx = canvas.getContext('2d');
+
 WIDTH = HEIGHT = canvas.width = canvas.height = 800;
 ctx.lineWidth = 1;
+const SCALE = 50;
+const DIMENSION = 16;
+const STRIPE_COUNT = triangularNumber(DIMENSION + 1);
+const COLOR_A = "#000";
+const COLOR_B = "#fff";
 
-const SCALE = 25;
-const DIMENSION = 32;
+function triangularNumber(n) {
+  return n * (n + 1) / 2;
+}
 
-// console.log(stripeEdges);
-//
-// var stripeColor = "black";
-// for (var z = 0; z < 500; z++) {
-//
-//   stripeColor = stripeColor === "black" ? "white" : "black";
-//   ctx.fillStyle = stripeColor === "white" ? "#fff" : "#000";
-//
-//   var zz = 0.5 * Math.sqrt(8 * z + 1) - (1/2);
-  // var difference = zz % SCALE;
-  // var nearestBorder = zz - difference;
-  //
-  // ctx.beginPath();
-  // ctx.moveTo((zz + 0) * SCALE * 2, 0);
-  // ctx.lineTo((zz + 1) * SCALE * 2, 0);
-  // ctx.lineTo(nearestBorder, difference + SCALE * 2);
-  // ctx.lineTo(nearestBorder, difference);
-  // ctx.closePath();
-  // ctx.fill();
+function inverseTriangularNumber(n) {
+  return 2 * (0.5 * Math.sqrt(8 * n + 1) - (1/2));
+}
 
-  // ctx.beginPath();
-  // ctx.moveTo((zz + 0) * SCALE * 2, 0);
-  // ctx.lineTo((zz + 1) * SCALE * 2, 0);
-  // ctx.lineTo(0, (zz + 1) * SCALE * 2);
-  // ctx.lineTo(0, (zz + 0) * SCALE * 2);
-  // ctx.closePath();
-  // ctx.fill();
-// }
-
-// //draw the striped squares
-// for (var i = 0; i < DIMENSION; i++) {
-//   let first
-//
-//
-//
-//
-//   //find the index of the first element of stripeEdges which would be inside this square (have to figure that out...? no we should just separately do diagonals? or more flexib)
-//   for (var x = i; x > 0; x--) {
-//     var y = i - x;
-//     drawStripedSquare(x, y, [stripes])
-//   }
-// }
-//
-//
-//
-//
-//
-// function stripedSquare(x, y) {
-//   //find the index of the first element of stripeEdges which would be inside this square (have to figure that out...? no we should just separately do diagonals? or more flexib)
-// }
-
-
-
-
-
-//draw the striped squares
-var stripeEdges = [];
-for (var z = 0; z < 555; z++) {
-  stripeEdges.push(Math.sqrt(8 * z + 1) - (1/2));
+var stripes = [];
+for (var n = 0; n < STRIPE_COUNT; n++) {
+  stripes.push(inverseTriangularNumber(n));
 }
 
 var i = 0;
 var thisDiagonalsStripeEdgeModuli = [0];
-var color = "#000";
+var color = COLOR_A;
 
-stripeEdges.forEach(function(stripeEdge) {
-  if (stripeEdge >= i + 2) {
-    // thisDiagonalsStripeEdgeModuli.push(1);
-    console.log('THIS DIAGONALS MODULI', thisDiagonalsStripeEdgeModuli);
-    color = color === "#000" ? "#fff" : "#000";
-    drawStripedSquareDiagonal(thisDiagonalsStripeEdgeModuli, i, color);
+stripes.forEach(function(stripe) {
+  if (stripe >= i + 2) {
+    // switchColor();
+    drawStripedSquareDiagonal(thisDiagonalsStripeEdgeModuli, i);
     thisDiagonalsStripeEdgeModuli = [0];
     i += 2;
   }
-  thisDiagonalsStripeEdgeModuli.push(stripeEdge % 2);
+  thisDiagonalsStripeEdgeModuli.push(stripe % 2);
 });
 
-function drawStripedSquareDiagonal(thisDiagonalsStripeEdgeModuli, diagonal, color) {
-
+function drawStripedSquareDiagonal(thisDiagonalsStripeEdgeModuli, diagonal) {
   for (var x = diagonal; x >= 0; x--) {
     var y = diagonal - x;
-    // console.log('****************CUR SQ OF DIAGONAL', x, y)
-    color = color === "#000" ? "#fff" : "#000";
-    drawStripedSquare(x, y, thisDiagonalsStripeEdgeModuli, color)
+    // switchColor();
+    drawStripedSquare(x, y, thisDiagonalsStripeEdgeModuli)
   }
 }
 
-function drawStripedSquare(x, y, thisDiagonalsStripeEdgeModuli, color) {
+function drawStripedSquare(x, y, thisDiagonalsStripeEdgeModuli) {
   thisDiagonalsStripeEdgeModuli.forEach(function(curModulus, index) {
-    color = color === "#000" ? "#fff" : "#000";
+    switchColor();
     ctx.fillStyle = color;
-
     var nextModulus = thisDiagonalsStripeEdgeModuli[index + 1] || 2;
-    // console.log('***CUR AND NEXT EDGE OF THIS SQ', curModulus, nextModulus)
-
     ctx.beginPath();
     ctx.moveTo((x + curModulus) * SCALE, y * SCALE);
-    // console.log((x + curModulus) * SCALE, y * SCALE)
     ctx.lineTo((x + nextModulus) * SCALE, y * SCALE);
-    // console.log((x + nextModulus) * SCALE, y * SCALE)
     ctx.lineTo(x * SCALE, (y + nextModulus) * SCALE)
-    // console.log(x * SCALE, (y + nextModulus) * SCALE)
     ctx.lineTo(x * SCALE, (y + curModulus) * SCALE);
-    // console.log(x * SCALE, (y + curModulus) * SCALE)
     ctx.closePath();
     ctx.fill();
   })
+  if (thisDiagonalsStripeEdgeModuli.length % 2 === 0) switchColor();
 }
 
-
-
-
-// draw the solid squares
 for (var x = 0; x < DIMENSION; x++) {
   for (var y = 0; y < DIMENSION; y++) {
-    if (x % 2 !== 0 && y % 2 === 0) solidSquare("black");
-    if (x % 2 === 0 && y % 2 !== 0) solidSquare("white");
+    if (x % 2 === 0 && y % 2 !== 0) solidSquare(COLOR_A);
+    if (x % 2 !== 0 && y % 2 === 0) solidSquare(COLOR_B);
   }
 }
+
 function solidSquare(color) {
-  ctx.fillStyle = color === "white" ? "#fff" : "#000";
+  ctx.fillStyle = color;
   ctx.beginPath();
   ctx.rect(x * SCALE, y * SCALE, SCALE, SCALE);
   ctx.fill();
+}
+
+function switchColor() {
+  color = color === COLOR_A ? COLOR_B : COLOR_A;
 }
