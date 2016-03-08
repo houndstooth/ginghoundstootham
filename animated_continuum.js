e0f38d133c48b8aa3b8d0bd2c579ee1bac832d56
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-03-08 08:18:44 -0800

    animation each square the same

--- animated_continuum.js
+++ animated_continuum.js
@@ -7,19 +7,22 @@ const ctx = canvas.getContext('2d');
 //user settings
 
 ctx.lineWidth = 1;
-const SCALE = 25;
-const DIMENSION = 32;
+const SCALE = 100;
+const DIMENSION = 8;
 WIDTH = HEIGHT = canvas.width = canvas.height = SCALE * DIMENSION;
-const STRIPE_COUNT = triangularNumber(DIMENSION + 1);
+const STRIPE_COUNT = 2000;
 const COLOR_A = "#000";
 const COLOR_B = "#fff";
-var THINNING_RATE = 1/4;
+var THINNING_RATE = 1/2;
 const FRAME_RATE = 1000/60;
 
 function triangularNumber(n) {
   return n * (n + 1) / 2;
 }
 
+function inverseTriangularNumber(n) {
+  return 0.5 * Math.sqrt(8 * n + 1) - 0.5;
+}
 
 // main loop
 
@@ -28,14 +31,10 @@ setInterval(function() {
 
   // animate
 
-  THINNING_RATE = THINNING_RATE * 1.001;
+  THINNING_RATE = THINNING_RATE * 1.005;
 
 
   //populate stripes
-
-  function inverseTriangularNumber(n) {
-    return 0.5 * Math.sqrt(8 * n + 1) - 0.5;
-  }
 
   var stripes = [];
   for (var n = 0; n < STRIPE_COUNT; n++) {
@@ -43,42 +42,26 @@ setInterval(function() {
   }
 
 
-  //from the master stripes, build and kick off each diagonal of striped squares
+  //draw squares
 
-  var i = 0;
-  var thisDiagonalsStripeEdgeModuli = [0];
-  stripes.forEach(function(stripe, index) {
-    if (stripe >= i + 2) {
-      drawStripedSquareDiagonal(thisDiagonalsStripeEdgeModuli, i);
-      thisDiagonalsStripeEdgeModuli = [0];
-      i += 2;
-    }
-
-    thisDiagonalsStripeEdgeModuli.push(stripe % 2);
-  });
-
-
-  //for a given diagonal, draw each of its striped squares
-
-  function drawStripedSquareDiagonal(thisDiagonalsStripeEdgeModuli, diagonal) {
-    for (var x = diagonal; x >= 0; x--) {
-      var y = diagonal - x;
-      drawStripedSquare(x, y, thisDiagonalsStripeEdgeModuli)
+  for (var x = 0; x < DIMENSION; x++) {
+    for (var y = 0; y < DIMENSION; y++) {
+      if (x % 2 !== 0 && y % 2 === 0) {
+        solidSquare(COLOR_A, x, y);
+      } else if (x % 2 === 0 && y % 2 !== 0) {
+        solidSquare(COLOR_B, x, y);
+      } else if (x % 2 === 0 && y % 2 === 0) {
+        stripedSquare(COLOR_A, x, y);
+      } else {
+        stripedSquare(COLOR_B, x, y);
+      }
     }
   }
 
-
-  //draw a single striped square
-
-  function switchColor(color) {
-    return color === COLOR_A ? COLOR_B : COLOR_A;
-  }
-
-  function drawStripedSquare(x, y, thisDiagonalsStripeEdgeModuli) {
-    var color = COLOR_A;
-    thisDiagonalsStripeEdgeModuli.forEach(function(curModulus, index) {
+  function stripedSquare(color, x, y) {
+    stripes.forEach(function(curModulus, index) {
       ctx.fillStyle = color;
-      var nextModulus = thisDiagonalsStripeEdgeModuli[index + 1] || 2;
+      var nextModulus = stripes[index + 1] || 2;
       ctx.beginPath();
       ctx.moveTo((x + curModulus) * SCALE, y * SCALE);
       ctx.lineTo((x + nextModulus) * SCALE, y * SCALE);
@@ -86,18 +69,8 @@ setInterval(function() {
       ctx.lineTo(x * SCALE, (y + curModulus) * SCALE);
       ctx.closePath();
       ctx.fill();
-      color = switchColor(color);
+      color = color === COLOR_A ? COLOR_B : COLOR_A;
     })
-  }
-
-
-  //draw the solid squares
-
-  for (var x = 0; x < DIMENSION; x++) {
-    for (var y = 0; y < DIMENSION; y++) {
-      if (x % 2 !== 0 && y % 2 === 0) solidSquare(COLOR_A, x, y);
-      if (x % 2 === 0 && y % 2 !== 0) solidSquare(COLOR_B, x, y);
-    }
   }
 
   function solidSquare(color, x, y) {
