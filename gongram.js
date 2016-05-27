56d7cc3261b77e007df32c31548fe5d022466ebf
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-05-27 13:08:12 -0700

    shepard check complete

--- gongram.js
+++ gongram.js
@@ -6,53 +6,42 @@ const SCALE = 50;
 const DIMENSION = 16;
 WIDTH = HEIGHT = canvas.width = canvas.height = SCALE * DIMENSION;
 
-const COLOR_A = "#000";
-const COLOR_B = "#00f";
-const COLOR_C = "#fff";
-const COLOR_D = "#0f0";
-const COLOR_E = "#f00";
+const COLOR_A = "#000000";
+const COLOR_B = "#0000ff";
+const COLOR_C = "#ffffff";
+const COLOR_D = "#00ff00";
+const COLOR_E = "#ff0000";
 
-var color, color_1, color_2;
+var color_1, color_2;
 
 const GINGHAM_MODE = false;
+const STRIPES_PER_SQUARE = 16;
 
 for (var x = 0; x < DIMENSION; x++) {
   for (var y = 0; y < DIMENSION; y++) {
+    if (x % 5 === 0) color_1 = COLOR_A;
+    if (x % 5 === 1) color_1 = COLOR_B;
+    if (x % 5 === 2) color_1 = COLOR_C;
+    if (x % 5 === 3) color_1 = COLOR_D;
+    if (x % 5 === 4) color_1 = COLOR_E;
+
+    if (y % 5 === 0) color_2 = COLOR_A;
+    if (y % 5 === 2) color_2 = COLOR_B;
+    if (y % 5 === 4) color_2 = COLOR_C;
+    if (y % 5 === 1) color_2 = COLOR_D;
+    if (y % 5 === 3) color_2 = COLOR_E;
+
     if (GINGHAM_MODE) {
-      if (x % 5 === 0) color_1 = COLOR_A;
-      if (x % 5 === 1) color_1 = COLOR_B;
-      if (x % 5 === 2) color_1 = COLOR_C;
-      if (x % 5 === 3) color_1 = COLOR_D;
-      if (x % 5 === 4) color_1 = COLOR_E;
-
-      if (y % 5 === 0) color_2 = COLOR_A;
-      if (y % 5 === 2) color_2 = COLOR_B;
-      if (y % 5 === 4) color_2 = COLOR_C;
-      if (y % 5 === 1) color_2 = COLOR_D;
-      if (y % 5 === 3) color_2 = COLOR_E;
-
-      color = mixColors(color_1, color_2);
-
-      solidSquare(color, x, y);
+      solidSquare(mixColors(color_1, color_2), x, y);
     } else {
-      if (x % 5 === 0) stripesForSquareWithTopLeftTriangle(COLOR_A, x, y);
-      if (x % 5 === 1) stripesForSquareWithTopLeftTriangle(COLOR_B, x, y);
-      if (x % 5 === 2) stripesForSquareWithTopLeftTriangle(COLOR_C, x, y);
-      if (x % 5 === 3) stripesForSquareWithTopLeftTriangle(COLOR_D, x, y);
-      if (x % 5 === 4) stripesForSquareWithTopLeftTriangle(COLOR_E, x, y);
-
-      if (y % 5 === 0) stripesForSquareWithBottomRightTriangle(COLOR_A, x, y);
-      if (y % 5 === 2) stripesForSquareWithBottomRightTriangle(COLOR_B, x, y);
-      if (y % 5 === 4) stripesForSquareWithBottomRightTriangle(COLOR_C, x, y);
-      if (y % 5 === 1) stripesForSquareWithBottomRightTriangle(COLOR_D, x, y);
-      if (y % 5 === 3) stripesForSquareWithBottomRightTriangle(COLOR_E, x, y);
+      stripedSquare(color_1, color_2, x, y);
     }
   }
 }
 
 function mixColors(color_1, color_2) {
   var result = "#";
-  for (i = 1; i < 4; i++) {
+  for (i = 1; i <= 6; i++) {
     if (color_1[i] === color_2[i]) {
       result += color_1[i];
     } else {
@@ -76,78 +65,55 @@ function solidSquare(color, x, y) {
   ctx.fill();
 }
 
-function stripesForSquareWithTopLeftTriangle(color, x, y) {
-  ctx.fillStyle = color;
-
-  ctx.beginPath();
-  ctx.moveTo(x * SCALE, y * SCALE);
-  ctx.lineTo((x + .5) * SCALE, y * SCALE);
-  ctx.lineTo(x * SCALE, (y + .5) * SCALE);
-  ctx.closePath();
-  ctx.fill();
-
-  ctx.beginPath();
-  ctx.moveTo((x + 1) * SCALE, y * SCALE);
-  ctx.lineTo((x + 1) * SCALE, (y + .5) * SCALE);
-  ctx.lineTo((x + .5) * SCALE, (y + 1) * SCALE);
-  ctx.lineTo(x * SCALE, (y + 1) * SCALE);
-  ctx.closePath();
-  ctx.fill();
+function scaledCoords(x, y) {
+  return [x * SCALE, y * SCALE];
 }
 
-function stripesForSquareWithBottomRightTriangle(color, x, y) {
-  ctx.fillStyle = color;
-
-  ctx.beginPath();
-  ctx.moveTo((x + .5) * SCALE, y * SCALE);
-  ctx.lineTo((x + 1) * SCALE, y * SCALE);
-  ctx.lineTo(x * SCALE, (y + 1) * SCALE);
-  ctx.lineTo(x * SCALE, (y + .5) * SCALE);
-  ctx.closePath();
-  ctx.fill();
-
-  ctx.beginPath();
-  ctx.moveTo((x + 1) * SCALE, (y + .5) * SCALE);
-  ctx.lineTo((x + 1) * SCALE, (y + 1) * SCALE);
-  ctx.lineTo((x + .5) * SCALE, (y + 1) * SCALE);
-  ctx.closePath();
-  ctx.fill();
+function moveTo(x, y) {
+  ctx.moveTo.apply(ctx, scaledCoords(x, y));
 }
 
-function stripedSquare(color_1, color_2, x, y, numStripes) {
+function lineTo(x, y) {
+  ctx.lineTo.apply(ctx, scaledCoords(x, y));
+}
+
+function stripedSquare(color_1, color_2, x, y) {
   ctx.fillStyle = color_1;
 
-  var unit = 2 / numStripes;
+  var unit = 2 / STRIPES_PER_SQUARE;
   var pos;
 
-  for (i = 0; i < numStripes; i++) {
+  for (i = 0; i < STRIPES_PER_SQUARE; i++) {
     pos = i * unit;
-
-    //if bigger or smaller than 1 ... around the bend of corner
-
     ctx.beginPath();
-    ctx.moveTo((x + pos) * SCALE, y * SCALE);
-    ctx.lineTo((x + pos + pos) * SCALE, y * SCALE);
-    ctx.lineTo()
+
+    if (pos <= 1) {
+      moveTo(x + pos, y);
+    } else {
+      moveTo(x + 1, y + (pos - 1));
+    }
+
+    if (pos <= 1) {
+      lineTo(x + pos + unit, y);
+    } else {
+      lineTo(x + 1, y + pos - 1 + unit);
+    }
+
+    if (pos <= 1) {
+      lineTo(x, y + pos + unit);
+    } else {
+      lineTo(x + pos - 1 + unit, y + 1);
+    }
+
+    if (pos <= 1) {
+      lineTo(x, y + pos);
+    } else {
+      lineTo(x + (pos - 1), y + 1);
+    }
 
     ctx.closePath();
     ctx.fill();
 
     ctx.fillStyle = ctx.fillStyle === color_1 ? color_2 : color_1;
   }
-
-  // ctx.beginPath();
-  // ctx.moveTo(x * SCALE, y * SCALE);
-  // ctx.lineTo((x + .5) * SCALE, y * SCALE);
-  // ctx.lineTo(x * SCALE, (y + .5) * SCALE);
-  // ctx.closePath();
-  // ctx.fill();
-  //
-  // ctx.beginPath();
-  // ctx.moveTo((x + 1) * SCALE, y * SCALE);
-  // ctx.lineTo((x + 1) * SCALE, (y + .5) * SCALE);
-  // ctx.lineTo((x + .5) * SCALE, (y + 1) * SCALE);
-  // ctx.lineTo(x * SCALE, (y + 1) * SCALE);
-  // ctx.closePath();
-  // ctx.fill();
 }
